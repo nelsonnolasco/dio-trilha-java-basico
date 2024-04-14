269cb66f48f3a77c37b4fd2402f779a65a9b6923
nelsonnolasco <nelsonnolasco@gmail.com>
2024-04-13 23:00:58 -0300

    Exercício Short, int com o Cast

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,9 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        
     }
 }
